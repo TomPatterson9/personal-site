3af220801d0fbb8ab7537a964ad24d2371706f29
TomPatterson9 <35941859+TomPatterson9@users.noreply.github.com>
2019-05-31 16:52:04 +0100

    updating hero

---
+++ script.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = "Welcome, I'm Tom.";
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("bannerTitle").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
