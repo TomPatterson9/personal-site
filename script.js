a7eff94aa3305b4bed684097e97ebbaa7e407ac7
TomPatterson9 <35941859+TomPatterson9@users.noreply.github.com>
2019-06-03 04:23:01 +0100

    bug fixes & added scroll up button

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function typeWriter() {
 }
 
 typeWriter();
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("topButton").style.display = "block";
+  } else {
+    document.getElementById("topButton").style.display = "none";
+  }
+}
+
+function scrollUp(){
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
