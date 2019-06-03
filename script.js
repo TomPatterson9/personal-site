577176882b4d8c79074851008f7240110b617eed
TomPatterson9 <35941859+TomPatterson9@users.noreply.github.com>
2019-06-03 18:20:58 +0100

    changed email icon

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ typeWriter();
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     document.getElementById("topButton").style.display = "block";
   } else {
     document.getElementById("topButton").style.display = "none";
