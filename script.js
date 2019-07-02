371d47ec3384a11e3a59f258f6394ca20118d4a0
TomPatterson9 <35941859+TomPatterson9@users.noreply.github.com>
2019-07-02 14:27:03 +0100

    added js to progress bars to trigger on scroll

--- script.js
+++ script.js
@@ -14,9 +14,20 @@ typeWriter();
 
 window.onscroll = function() {scrollFunction()};
 
+
+
 function scrollFunction() {
+
   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     document.getElementById("topButton").style.display = "block";
+      const progressBarsArray = document.querySelectorAll('.progress66, .progress33, .progress100');
+
+    progressBarsArray.forEach(element => {
+      element.style.animationPlayState = "running";
+      element.style.WebkitAnimationPlayState = "running";
+
+    })
+
   } else {
     document.getElementById("topButton").style.display = "none";
   }
@@ -26,3 +37,4 @@ function scrollUp(){
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
