19a9c8f7eaa5e212a05631545130628c5cc59ec8
Tom Patterson <thomas.patterson99@Gmail.com>
2020-06-07 01:07:46 +0100

    changed project layout

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ function scrollUp(){
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("imageSlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
